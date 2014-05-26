c1cf56b1965bd6593ad08d55b9947dfa0fe7a12b
Ian Noyes <ian@12southmusic.com>
2014-05-25 23:06:35 -0500

    add app.css to the assets folder

--- lib/custom.php
+++ lib/custom.php
@@ -2,3 +2,7 @@
 /**
  * Custom functions
  */
+function custom_scripts() {
+  wp_enqueue_style('roots_custom', get_template_directory_uri() . '/assets/css/app.css');
+}
+add_action('wp_enqueue_scripts', 'custom_scripts', 200);
